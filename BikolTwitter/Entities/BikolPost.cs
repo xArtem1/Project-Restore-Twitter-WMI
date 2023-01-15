68cce39173db05aa3898521ea4f2545785abfa18
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-01-15 11:02:37 +0100

    create bikolposts table

---
+++ BikolTwitter/Entities/BikolPost.cs
@@ -0,0 +1,33 @@
+﻿namespace BikolTwitter.Entities;
+
+/// <summary>
+/// Class representing twitter post, wchich was sold by a customer.
+/// This is an database entity.
+/// </summary>
+public class BikolPost
+{
+    /// <summary>
+    /// Primary key.
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// Identifier of twitter post.
+    /// </summary>
+    public string TwitterId { get; set; }
+    /// <summary>
+    /// Date of post publication.
+    /// </summary>
+    public DateTime Date { get; set; }
+    /// <summary>
+    /// Amount the post was sold for.
+    /// </summary>
+    public decimal Profit { get; set; }
+    /// <summary>
+    /// Foreign key to bikolsub.
+    /// </summary>
+    public int BikolSubId { get; set; }
+    /// <summary>
+    /// Bikolsub included by foreign key.
+    /// </summary>
+    public virtual BikolSub BikolSub { get; set; }
+}
---
+++ BikolTwitter/Migrations/20230115100107_AddBikolPostsTable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BikolTwitter.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBikolPostsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BikolPosts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TwitterId = table.Column<string>(type: "TEXT", nullable: true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Profit = table.Column<decimal>(type: "decimal(5,2)", nullable: false),
+                    BikolSubId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BikolPosts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BikolPosts_BikolSubs_BikolSubId",
+                        column: x => x.BikolSubId,
+                        principalTable: "BikolSubs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BikolPosts_BikolSubId",
+                table: "BikolPosts",
+                column: "BikolSubId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BikolPosts_TwitterId",
+                table: "BikolPosts",
+                column: "TwitterId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BikolPosts");
+        }
+    }
+}
